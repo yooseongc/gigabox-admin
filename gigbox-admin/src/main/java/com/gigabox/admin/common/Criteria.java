5bbac8d6082818bde4db6c1575fb0cd3d4655bc2
yooseongc <choiys3574@naver.com>
2017-06-13 09:24:07 +0900

    유성 170613 오전 커밋 : 영화 관리 시작

---
+++ gigbox-admin/src/main/java/com/gigabox/admin/common/Criteria.java
@@ -0,0 +1,53 @@
+package com.gigabox.admin.common;
+
+public class Criteria {
+	
+	private int page;
+	private int perPageNum;
+	
+	public Criteria() {
+		this.page = 1;
+		this.perPageNum = 10;
+	}
+	
+	public void setPage(int page) {
+		if (page <= 0) {
+			this.page = 1;
+			return;
+		}
+		this.page = page;
+	}
+	
+	public void setPerPageNum(int perPageNum) {
+		if (perPageNum <= 0 || perPageNum > 100) {
+			this.perPageNum = 10;
+			return;
+		}
+		this.perPageNum = 10;
+	}
+	
+	public int getPage() {
+		return page;
+	}
+	
+	// For Mapper
+	public int getPerPageNum() {
+		return this.perPageNum;
+	}
+	
+	// For Mapper
+	public int getPageStart() {
+		return (this.page - 1) * perPageNum;
+	}
+	
+	// For Mapper
+	public int getPageEnd() {
+		return this.page * perPageNum;
+	}
+	
+	@Override
+	public String toString() {
+		return "Creteria [page=" + page + ", perPageNum=" + perPageNum + "]";
+	}
+	
+}
---
+++ gigbox-admin/src/main/java/com/gigabox/admin/movie/vo/MovieVO.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.gigabox.admin.movie.vo;
+
+import com.gigabox.admin.common.Criteria;
+
+
+public class MovieVO extends Criteria {
+	
+	
+	
+}
